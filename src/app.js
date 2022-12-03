8ee360ce3116e29b1dbd32450f0cbe7f842ac144
Luis Ramos <luissquall@gmail.com>
2022-12-03 10:29:43 -0600

    Return contract only if it belongs to the profile calling

--- src/app.js
+++ src/app.js
@@ -10,13 +10,29 @@ app.set('models', sequelize.models)
 app.use(pino)
 
 /**
- * FIX ME!
- * @returns contract by id
+ * Return a contract by id
+ *
+ * It should return the contract only if it belongs to the profile calling
+ *
+ * @returns contract
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
+    const { Op } = require('sequelize');
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    // We could have found the contract, even if it was not associated to the profile calling,
+    // and then thrown an unauthorized error. I decided not using that strategy to avoid hinting the
+    // existence of records that don't belong to you
+    const contract = await Contract.findOne({
+        where: {
+            id,
+            [Op.or]: [
+                { ContractorId: req.profile.id },
+                { ClientId: req.profile.id }
+            ]
+        }
+    })
+
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
