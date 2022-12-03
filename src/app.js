b76d881deb112dd341f37863e80a5c9e425e7693
Luis Ramos <luissquall@gmail.com>
2022-12-03 10:01:37 -0600

    Add pino logger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')()
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
+app.use(pino)
 
 /**
  * FIX ME!
